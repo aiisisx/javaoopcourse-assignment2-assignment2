02887dc610fb689650bd069ac2f83f63e9b3b983
Asylkan Osmonalieva <osmonalievaasylkan@gmail.com>
2026-02-10 11:10:22 +0600

    Enhance Library class with book management methods
    Implemented methods to add, display, search, borrow, and return books in the library.

--- src/Library.java
+++ src/Library.java
@@ -1,35 +1,59 @@
 public class Library {
-
     private Book[] books;
     private int count;
 
     public Library(int capacity) {
         books = new Book[capacity];
-        count = 0;
+        count =0;
     }
 
-    // TODO: Add book to array
     public void addBook(Book book) {
-        // implement
+        if (count < books.length) {
+            books[count] = book;
+            count++;
+            System.out.println("Book added successfully");
+        } else {
+            System.out.println("Library is full");
+        }
     }
 
-    // TODO: Display all books
     public void displayBooks() {
-        // implement
+        if (count == 0) {
+            System.out.println("No books in the library");
+            return;
+        }
+
+        for (int i=0; i < count; i++) {
+            System.out.println(books[i]);
+        }
     }
 
-    // TODO: Search book by title
     public Book searchByTitle(String title) {
+        for (int i=0; i<count; i++) {
+            if (books[i].getTitle().equalsIgnoreCase(title)) {
+                return books[i];
+            }
+        }
         return null;
     }
 
-    // TODO: Borrow book by title
     public void borrowBook(String title) {
-        // implement
+        Book book = searchByTitle(title);
+        if (book != null && book.isAvailable()) {
+            book.borrowBook();
+            System.out.println("Book borrowed!");
+        } else {
+            System.out.println("Book not available or not found.");
+        }
     }
 
-    // TODO: Return book by title
     public void returnBook(String title) {
-        // implement
+        Book book = searchByTitle(title);
+        if (book != null) {
+            book.returnBook();
+            System.out.println("Book returned!");
+        } else {
+            System.out.println("Book not found.");
+        }
     }
 }
