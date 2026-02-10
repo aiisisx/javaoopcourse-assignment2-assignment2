f40a8297b6e95a62b3bcd8de686f1e76c11e511f
Asylkan Osmonalieva <osmonalievaasylkan@gmail.com>
2026-02-10 11:09:32 +0600

    Implement EBook constructor and toString method

--- src/EBook.java
+++ src/EBook.java
@@ -1,16 +1,14 @@
 public class EBook extends Book {
-
     private double fileSize;
 
-    // TODO: Implement constructor using super()
-    public EBook(String title, String author, int year, double fileSize) {
+    public EBook(String title, String author, int year, double fileSize ) {
         super(title, author, year);
-        // implement
+        this.fileSize = fileSize;
     }
 
-    // TODO: Override toString()
     @Override
     public String toString() {
-        return "";
+        return "[E-Book] " + super.toString() +
+                ", File Size: " + fileSize + "MB";
     }
 }
